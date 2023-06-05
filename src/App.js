823f10dfad5adc3c51a91ca137d8360836d21681
sarthak503 <sarthak.kumar503@gmail.com>
2023-06-05 14:15:50 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    try {
+      const response = await fetch('/api/tasks'); // Replace with your backend API endpoint
+      const data = await response.json();
+      setTasks(data);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+    }
+  };
+
+  const addTask = async (newTask) => {
+    try {
+      const response = await fetch('/api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTask)
+      });
+      const data = await response.json();
+      setTasks([...tasks, data]);
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  };
+
+  const updateTaskStatus = async (taskId) => {
+    try {
+      const response = await fetch(`/api/tasks/${taskId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ status: 'completed' })
+      });
+      if (response.ok) {
+        const updatedTasks = tasks.map((task) =>
+          task.id === taskId ? { ...task, status: 'completed' } : task
+        );
+        setTasks(updatedTasks);
+      }
+    } catch (error) {
+      console.error('Error updating task status:', error);
+    }
+  };
+
+  const deleteTask = async (taskId) => {
+    try {
+      const response = await fetch(`/api/tasks/${taskId}`, {
+        method: 'DELETE'
+      });
+      if (response.ok) {
+        const filteredTasks = tasks.filter((task) => task.id !== taskId);
+        setTasks(filteredTasks);
+      }
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
   return (
-    <div>App</div>
-  )
-}
+    <div className="App">
+      <h1>Task Management</h1>
+      <TaskForm addTask={addTask} />
+      <h2>Tasks:</h2>
+      {tasks.length === 0 ? (
+        <p>No tasks available</p>
+      ) : (
+        <TaskList
+          tasks={tasks}
+          updateTaskStatus={updateTaskStatus}
+          deleteTask={deleteTask}
+        />
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
